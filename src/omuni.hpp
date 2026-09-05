446e22ad832b52cd0854ac5ab0cadb4fcf5c7dbf
zassou-robo <zassou.sub@gmail.com>
2026-09-05 10:16:49 +0900

    オムニ制御ファイル追加

---
+++ src/omuni.hpp
@@ -0,0 +1,42 @@
+#ifndef OMUNI_HPP
+#define OMUNI_HPP
+
+#include <mbed.h> 
+
+struct omuni_param{
+    int const_output_drive;
+    int const_output_turn;
+};
+
+class Omuni{
+    public:
+    Omuni(const omuni_param const_output_): const_output_(const_output_){};
+    void omuni_calc(int8_t lstick_x,int8_t lstick_y,int8_t rstick_x,int16_t goal[4]){
+        int16_t drive_goal[4] = {0};
+        int16_t turn_goal[4] = {0};
+            if(abs(lstick_x) > 20 ||abs(lstick_y) > 20){
+                for(int i = 0;i < 4;i++){
+                    drive_goal[i] = sin(atan2(lstick_y,lstick_x) + (i*2 + 1) * M_PI / 4) * hypot(lstick_x,lstick_y) / 127 * const_output_.const_output_drive;
+                }
+            }else{
+                for(int i = 0;i < 4;i ++){
+                    drive_goal[i] = 0;
+                }
+            }
+            if(abs(rstick_x) > 20){
+                for(int i = 0;i < 4;i ++){
+                    turn_goal[i] = rstick_x / 127 * const_output_.const_output_turn;
+                }
+            }else{
+                for(int i = 0;i < 4; i++){
+                    turn_goal[i] = 0;
+                }
+            }
+            for(int i = 0; i < 4;i ++){
+                goal[i] = drive_goal[i] + turn_goal[i];
+            }
+        }
+    private:
+    omuni_param const_output_;
+};
+#endif 
